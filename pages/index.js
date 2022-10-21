c2dd1043012540493927efd345b007076eae449e
Onur Dogan <iamonurdogan@gmail.com>
2022-10-21 20:35:33 +0300

    dark mode

--- pages/index.js
+++ pages/index.js
@@ -33,12 +33,12 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons">onur dogan</h1>
+            <h1 className="text-xl font-burtons dark:text-white">onur dogan</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className="cursor-pointer text-2xl"
+                  className="cursor-pointer text-2xl dark:text-white"
                 />
               </li>
               <li>
