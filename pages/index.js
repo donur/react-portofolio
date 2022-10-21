da7f41f555ecaa576143ff14d5ef74636bda41dc
Onur Dogan <iamonurdogan@gmail.com>
2022-10-21 16:54:44 +0300

    portfolio

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,12 @@ import deved from "../public/dev-ed-wave.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 export default function Home() {
   return (
@@ -76,41 +82,88 @@ export default function Home() {
               programming and teaching.
             </p>
           </div>
-          <div className="text-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={design} width={100} height={"100"} />
-            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-            <p className="py-2">
-              Creating elegant designs suited for your needs following core
-              design theory.
-            </p>
-            <h4 className="py-4 text-teal-600">Desing tools I use</h4>
-            <p className="text-gray-800 py-1">Adobe</p>
-            <p className="text-gray-800 py-1">Illustrator</p>
-            <p className="text-gray-800 py-1">Figma</p>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={design} width={100} height={"100"} />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Desing tools I use</h4>
+              <p className="text-gray-800 py-1">Adobe</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={code} width={100} height={"100"} />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Code your dream project
+              </h3>
+              <p className="py-2">
+                Do you have an idea for your next great website? Let's make it a
+                reality.
+              </p>
+              <h4 className="py-4 text-teal-600">Desing tools I use</h4>
+              <p className="text-gray-800 py-1">Adobe</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={consulting} width={100} height={"100"} />
+              <h3 className="text-lg font-medium pt-8 pb-2">Consulting</h3>
+              <p className="py-2">
+                Are you interested in feedback for your current project? I can
+                give you tips and tricks to level it up.
+              </p>
+              <h4 className="py-4 text-teal-600">Desing tools I use</h4>
+              <p className="text-gray-800 py-1">Adobe</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
           </div>
-          <div className="text-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={code} width={100} height={"100"} />
-            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-            <p className="py-2">
-              Creating elegant designs suited for your needs following core
-              design theory.
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
             </p>
-            <h4 className="py-4 text-teal-600">Desing tools I use</h4>
-            <p className="text-gray-800 py-1">Adobe</p>
-            <p className="text-gray-800 py-1">Illustrator</p>
-            <p className="text-gray-800 py-1">Figma</p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
           </div>
-          <div className="text-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={consulting} width={100} height={"100"} />
-            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
-            <p className="py-2">
-              Creating elegant designs suited for your needs following core
-              design theory.
-            </p>
-            <h4 className="py-4 text-teal-600">Desing tools I use</h4>
-            <p className="text-gray-800 py-1">Adobe</p>
-            <p className="text-gray-800 py-1">Illustrator</p>
-            <p className="text-gray-800 py-1">Figma</p>
+          <div>
+            <div>
+              <Image src={web1} />
+            </div>
+            <div>
+              <Image src={web2} />
+            </div>
+            <div>
+              <Image src={web3} />
+            </div>
+            <div>
+              <Image src={web4} />
+            </div>
+            <div>
+              <Image src={web5} />
+            </div>
+            <div>
+              <Image src={web6} />
+            </div>
           </div>
         </section>
       </main>
